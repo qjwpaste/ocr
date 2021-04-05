046f6b8e9d80bd13903c0952970e3800da79a347
qjwpaste <48791708+qjwpaste@users.noreply.github.com>
2021-04-05 11:51:29 +0000

    Update BodyInfoUtils.java
    1

--- src/main/java/com/yaxi/ocr/ocr/util/BodyInfoUtils.java
+++ src/main/java/com/yaxi/ocr/ocr/util/BodyInfoUtils.java
@@ -57,7 +57,7 @@ public class BodyInfoUtils {
         String method = "POST";
 
         Map<String, String> headers = new HashMap<String, String>();
-        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE ###
         headers.put("Authorization", "APPCODE " + appcode);
         //根据API的要求，定义相对应的Content-Type
         headers.put("Content-Type", "application/json; charset=UTF-8");
