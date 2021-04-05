81359d9d972f67ab9aa775f72b4d7b1d9f935801
qjwpaste <48791708+qjwpaste@users.noreply.github.com>
2021-04-05 11:48:39 +0000

    Update OcrApplicationTests.java
    1

--- src/test/java/com/yaxi/ocr/ocr/OcrApplicationTests.java
+++ src/test/java/com/yaxi/ocr/ocr/OcrApplicationTests.java
@@ -53,12 +53,12 @@ public class OcrApplicationTests {
     public void test1() {
         String host = "http://dm-51.data.aliyun.com";
         String path = "/rest/160601/ocr/ocr_idcard.json";
-        String appcode = "84c77271340846398725662013268a8e";
+        String appcode = "###";
         String imgFile = "C:\\Users\\Administrator\\Desktop\\1.png";
         String method = "POST";
 
         Map<String, String> headers = new HashMap<String, String>();
-        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE ###
         headers.put("Authorization", "APPCODE " + appcode);
         //根据API的要求，定义相对应的Content-Type
         headers.put("Content-Type", "application/json; charset=UTF-8");
